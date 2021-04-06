d23322e6a48c3c28b12110cd41aafe4c3f57b46a
lucasjesusone <lucasjesusone@gmail.com>
2021-04-05 22:36:42 -0400

    Criação Componente principal

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import './Main.scss'
 
 const Main = () => {
+  return (
+    <div>
+      <div>
+        <p>01</p>
+        <h2>Interface Amigável</h2>
+        <p>
+          O app Minv foi construído para ser fácil e simples em qualquer app
+        </p>
+      </div>
+      <div>
+        <p>01</p>
+        <h2>Interface Amigável</h2>
+        <p>
+          O app Minv foi construído para ser fácil e simples em qualquer app
+        </p>
+      </div>
+      <div>
+        <p>01</p>
+        <h2>Interface Amigável</h2>
+        <p>
+          O app Minv foi construído para ser fácil e simples em qualquer app
+        </p>
+      </div>
+      <div>
+        <p>01</p>
+        <h2>Interface Amigável</h2>
+        <p>
+          O app Minv foi construído para ser fácil e simples em qualquer app
+        </p>
+      </div>
+      <div>
+        <p>01</p>
+        <h2>Interface Amigável</h2>
+        <p>
+          O app Minv foi construído para ser fácil e simples em qualquer app
+        </p>
+      </div>
+    </div>
+  );
+};
 
-        return (
-           <div>
-               
-           </div>
-        )
-         
-}
-
-
-
-export default Main
+export default Main;

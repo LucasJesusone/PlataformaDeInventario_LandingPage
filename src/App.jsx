12d364bdd731e20515659f31ab9d63992c37ee8e
lucasjesusone <lucasjesusone@gmail.com>
2021-04-05 21:16:40 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Nav from './NavBar/Nav';
+
+function App() {
+  return (
+
+      <Nav/>
+
+  );
+}
+
+export default App;
---
+++ src/NavBar/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './navBar.css'
+
+const NavBar = () => {
+    return (
+       <>
+        <nav className="navbar">
+            <div>
+                <h1>Cotar-me</h1>
+            </div>
+            <ul>
+                <a 
+                href="https://www.youtube.com/watch?v=L7Rh_gkJT6M"
+                target="_blank"
+                rel="noreferrer">  
+                </a>
+
+                <button>Login</button>
+            </ul>
+        </nav>
+       </>
+    )
+        
+    
+}
+
+export default NavBar;

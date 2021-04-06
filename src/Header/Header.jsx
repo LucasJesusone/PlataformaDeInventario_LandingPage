a14779b9ab61dca4b0b9ba15d17ca4b82810d3b2
lucasjesusone <lucasjesusone@gmail.com>
2021-04-05 22:37:05 -0400

    Criação Componente header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => {
+
+        return (
+           <div className="main">
+               <h1>Plataforma de Inventário</h1>
+           </div>
+        )
+         
+}
+
+
+
+export default Header

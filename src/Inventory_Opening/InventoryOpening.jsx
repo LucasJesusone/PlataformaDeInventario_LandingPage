b4707035882d8672db39f19e36963c9995792fcb
lucasjesusone <lucasjesusone@gmail.com>
2021-04-12 23:36:41 -0400

    Criação componente de inventário

---
+++ src/Inventory_Opening/InventoryOpening.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './InventoryOpening.scss'
+
+const Inventory = () => {
+
+        return (
+            <> 
+          <div className="style">
+              <h1>Abertura de Inventário</h1>
+          </div>
+          <div>
+              <p><strong>Faça Abertura/encerramento dos inventários</strong> diretamente do SAP <strong>100% integrado</strong> com a aplicação mobile Minv</p>
+          </div>
+          </>
+        )
+  
+}
+
+export default Inventory;

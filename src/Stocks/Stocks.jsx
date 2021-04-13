857b3b5859de9dc0e34b2bb20ad6a9761a1c1fc4
lucasjesusone <lucasjesusone@gmail.com>
2021-04-12 23:37:27 -0400

    Esqueleto inicial

---
+++ src/Stocks/Stocks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Stocks.scss";
+
+const Stocks = () => {
+  return (
+    <div className="main">
+      <h1>Inventário exemplo</h1>
+    </div>
+  );
+};
+
+export default Stocks;

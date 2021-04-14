a361e7f48f79c45143cbdd481a19777eb8439ade
lucasjesusone <lucasjesusone@gmail.com>
2021-04-13 23:53:09 -0400

    Começando estilização front

--- src/Inventory_Opening/InventoryOpening.jsx
+++ src/Inventory_Opening/InventoryOpening.jsx
@@ -1,19 +1,27 @@
-import React, { Component } from 'react'
-import './InventoryOpening.scss'
+import React from "react";
+
+
+import "./InventoryOpening.scss";
 
 const Inventory = () => {
-
-        return (
-            <> 
-          <div className="style">
-              <h1>Abertura de Inventário</h1>
-          </div>
-          <div>
-              <p><strong>Faça Abertura/encerramento dos inventários</strong> diretamente do SAP <strong>100% integrado</strong> com a aplicação mobile Minv</p>
-          </div>
-          </>
-        )
-  
-}
+  return (
+    <div className="main">
+      <div className="style">
+        <h1>
+          Abertura <br />
+          de Inventário
+        </h1>
+        <div className='text'>
+          <p>
+            <strong>Faça Abertura/encerramento dos inventários </strong>
+            <br/>diretamente do SAP <strong>100% integrado </strong><br/>com a aplicação
+            mobile Minv
+          </p>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
 
 export default Inventory;

00028b084a1b476a02f2baa5111a84273d472b10
lucasjesusone <lucasjesusone@gmail.com>
2021-04-15 01:58:13 -0400

    feat: styles

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,44 +3,58 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="head">
-        <h1 className="title-header">Plataforma de Inventário</h1>
-      </div>
+    <>
       <div className="services">
+        <h2>Plataforma de Inventário</h2>
         <div className="service-column">
           <h2>INTERFACE AMIGÁVEL</h2>
-          <p>
-            O app Minv foi construído para ser fácil e simples em qualquer app
-          </p>
+          <span>
+            {" "}
+            <p>
+              O app Minv foi construído para ser fácil e simples em qualquer app
+            </p>
+          </span>
         </div>
         <div className="service-column">
           <h2>100% INTEGRADO</h2>
-          <p>
-            Construímos todas interfaces no seu SAP para você não se preocupar
-            com integrações
-          </p>
+          <span>
+            {" "}
+            <p>
+              Construímos todas interfaces no seu SAP para você não se preocupar
+              com integrações{" "}
+            </p>
+          </span>
         </div>
         <div className="service-column">
           <h2>SUPORTE OFFLINE</h2>
-          <p>
-            Realize o inventário em áreas sem acesso à internet e quando estiver
-            online, integre facilmente os dados
-          </p>
+          <span>
+            {" "}
+            <p>
+              Realize o inventário em áreas sem acesso à internet e quando
+              estiver online, integre facilmente os dados
+            </p>
+          </span>
         </div>
         <div className="service-column">
           <h2>USUÁRIOS ILIMITADOS</h2>
-          <p>Disponibilizamos quantos usuários você precisar, sem limitações</p>
+          <span>
+            <p>
+              Disponibilizamos quantos usuários você precisar, sem limitações
+            </p>
+          </span>
         </div>
         <div className="service-column">
           <h2>RELATÓRIOS</h2>
-          <p>
-            Todos relatórios podem ser extraídos diretamente do SAP em tempo
-            real
-          </p>
+          <span>
+            <p>
+              {" "}
+              Todos relatórios podem ser extraídos diretamente do SAP em tempo
+              real
+            </p>
+          </span>
         </div>
       </div>
-    </header>
+    </>
   );
 };
 

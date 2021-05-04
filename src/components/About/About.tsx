cdcd1789935d96cd113e4f86b32c7ca71410f4d7
lucasjesusone <lucasjesusone@gmail.com>
2021-05-04 00:53:41 -0400

    styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -13,36 +13,34 @@ import headIcon from '../../images/head-icon.png';
 const About: React.FC = () => {
 	return (
 		<Container className="article-container" id="About">
-			<Title title="About us">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do&nbsp;eiusmod tempor incididunt ut&nbsp;labore
-				et&nbsp;dolore magna aliqua. Ut&nbsp;enim ad&nbsp;minim veniam,
+			<Title title="Implementação">
 			</Title>
-			<Row className="justify-content-between">
-				<Col sm={4} xs={12}>
+			<Row className="space-around">
+				<Col sm={3} xs={12}>
 					<div className={s.aboutItem}>
 						<img src={internetIcon} alt="internetIcon" />
-						<div className={cx(s.title, 'textSemibold')}>Awesome Icons</div>
-						<div className={s.subtitle}>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-						</div>
+						<div className={cx(s.title, 'textSemibold')}>LEVANTAMENTO DE REQUISITOS</div>
+	
 					</div>
 				</Col>
-				<Col sm={4} xs={12}>
+				<Col sm={8} xs={12}>
 					<div className={s.aboutItem}>
 						<img src={transferIcon} alt="transferIcon" />
-						<div className={cx(s.title, 'textSemibold')}>One Page</div>
-						<div className={s.subtitle}>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-						</div>
+						<div className={cx(s.title, 'textSemibold')}>IMPLEMENTAÇÕES DE SERVIÇOS/INTEGRAÇÕES SAP</div>
+					</div>
+				</Col>
+				<Col sm={8} xs={1} >
+					<div className={s.aboutItem}>
+						<img src={headIcon} alt="headIcon" />
+						<div className={cx(s.title, 'textSemibold')}>FASE DE TESTES </div>
+					
 					</div>
 				</Col>
 				<Col sm={4} xs={12} >
 					<div className={s.aboutItem}>
 						<img src={headIcon} alt="headIcon" />
-						<div className={cx(s.title, 'textSemibold')}>Fully Responsive</div>
-						<div className={s.subtitle}>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-						</div>
+						<div className={cx(s.title, 'textSemibold')}>GO-LIVE EM PRODUÇÃO</div>
+					
 					</div>
 				</Col>
 			</Row>

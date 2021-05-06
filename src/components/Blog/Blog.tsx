644f7b91d6dec7951d085a20990cb3d8f06350ad
lucasjesusone <lucasjesusone@gmail.com>
2021-05-05 23:28:47 -0400

    Setting informations about Landing

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,16 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import cx from 'classnames'
-import s from './Blog.module.sass';
+import cx from 'classnames';
+import style from './Blog.module.sass';
 import Container from 'react-bootstrap/Container';
 import Title from '../Title';
 import BlogArticle from './BlogArticle';
 import { BlogArticleProps } from './BlogArticle/BlogArticle';
-import article1 from '../../images/article-1.png';
-import article2 from '../../images/article-2.png';
-import article3 from '../../images/article-3.png';
+import image from '../../images/image--017.png';
+import image1 from '../../images/image--014.png';
+import image2 from '../../images/image--023.png';
+import image3 from '../../images/image2.png';
 import Row from 'react-bootstrap/Row';
-import Button from '../UI/Button';
+// import Button from '../UI/Button';
 import Col from 'react-bootstrap/Col';
 
 interface BlogComponent {
@@ -19,26 +20,26 @@ interface BlogComponent {
 
 const Blog: React.FC & BlogComponent = () => {
 	return (
-		<div className={cx(s.blogContainer, 'article-container')} id="Blog">
-			<Container>
-				<Title title="Blog">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do&nbsp;eiusmod tempor incididunt ut&nbsp;labore
-					et&nbsp;dolore magna aliqua. Ut&nbsp;enim ad&nbsp;minim veniam,
-				</Title>
+		<>
+			<div className={cx(style.blogContainer, 'article-container')} id="Blog">
+				<Container>
+				
+					<Row className={style.image1}>
+							<img src={image1} alt="" width="600" />
 
-				<BlogArticle title="SEO Quis Vestibulum" date="04.06.2020" type="Development" image={article1}>
-					Dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-					commodo consequat...
-				</BlogArticle>
-				<BlogArticle title="SEO Quis Vestibulum" date="04.06.2020" type="Development" image={article2}>
-					Dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-					commodo consequat...
-				</BlogArticle>
-				<BlogArticle title="SEO Quis Vestibulum" date="04.06.2020" type="Development" image={article3}>
-					Dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-					commodo consequat...
-				</BlogArticle>
-				<Row>
+					</Row>
+					<Row className={style.image} >
+						<img src={image} alt="" width="280" />
+						<Title
+						title="ABERTURA DE 
+						INVENTÁRIO"
+					>
+						<strong>Faça abertura/encerramento dos inventários</strong> diretamente do SAP{' '}
+						<strong>100% integrado</strong> com a aplicação mobile MINV
+					</Title>
+					</Row>
+
+					{/* <Row>
 					<Col>
 						<div className={cx(s.linkContainer, 'd-flex', 'justify-content-center')}>
 							<Button square green big link>
@@ -46,9 +47,36 @@ const Blog: React.FC & BlogComponent = () => {
 							</Button>
 						</div>
 					</Col>
-				</Row>
-			</Container>
-		</div>
+				</Row> */}
+				</Container>
+			</div>
+			<div className={cx(style.blogContainer, 'article-container')} id="Blog">
+				<Container>
+					<Row>
+						<Row md={7} className={style.image3}>
+							<img src={image3} alt="" width="280" height="500" />
+						</Row>
+						<Col md={5} className={style.image2}>
+							<img src={image2} alt="" width="280" />
+							
+						</Col>
+						<Title title="CRIAÇÃO DE
+						OPERAÇÃO">
+							<strong>Crie Facilmente operações de</strong><br/><strong>inventário/baixa</strong> utilizando o leitor <br/>de código de barras
+						</Title>
+					</Row>
+					{/* <Row>
+					<Col>
+						<div className={cx(s.linkContainer, 'd-flex', 'justify-content-center')}>
+							<Button square green big link>
+								More View
+							</Button>
+						</div>
+					</Col>
+				</Row> */}
+				</Container>
+			</div>
+		</>
 	);
 };
 

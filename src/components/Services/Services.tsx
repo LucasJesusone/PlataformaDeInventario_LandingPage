eadcc04a7b05b73bc93b77233b6ae86a736fecdf
lucasjesusone <lucasjesusone@gmail.com>
2021-05-12 20:44:11 -0400

    Main Card

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Title from '../Title';
+
 import ServicesCard from './ServicesCard';
 import { ServicesCardProps } from './ServicesCard/ServicesCard';
 import style from './Services.module.scss';
@@ -15,8 +15,7 @@ const Services: React.FC & ServicesComponent = () => {
 	return (
 		<div className={cx(style.serviceContainer, 'article-container')} id="Services">
 			<Container>
-				<Title title="PLATAFORMA DE INVENTÁRIO">
-				</Title>
+		
 				<Row>
 					<Services.Card title="Interface Amigável">
 						O aplicativo Minv foi construido para ser simples e fácil de utilizar em qualquer aparelho <strong>Android</strong>

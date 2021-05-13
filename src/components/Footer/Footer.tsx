ed721560e7afeecb834e0d01454b0469e3796cdc
lucasjesusone <lucasjesusone@gmail.com>
2021-05-12 20:43:54 -0400

    adding contact infos

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import style from './Footer.module.sass';
+import { AiOutlineMail,} from 'react-icons/ai';
+import { FiLinkedin } from 'react-icons/fi';
+
 
 const Footer: React.FC = () => {
 	const year = new Date().getFullYear();
 
 	return (
 		<>
-			<div className={style.footer} >
+			<div className={style.footer}>
 				<span className="textBold"> Todos os direitos Reservados © {year}</span>
 			</div>
-			<div className={style.footer} >
-				<span className="textBold">eniorj1@hotmail.com</span>
+			{/* <span className={style.contact}>Entre em Contato</span> */}
+			<div className={style.footer}>
+				
+				<a className={style.icon} href="mailto:eniorj1@hotmail.com?body=Olá, Tudo bem ?">
+					<AiOutlineMail size={40} title="Email" />
+				</a>
+				<a className={style.icon} href="https://www.linkedin.com/in/eniorafael/">
+					<FiLinkedin size={37} />
+				</a>
 			</div>
 		</>
 	);

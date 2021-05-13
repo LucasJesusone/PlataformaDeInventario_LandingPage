d14eb5f67e5ac88f111af9721ff64a80d9c88cee
lucasjesusone <lucasjesusone@gmail.com>
2021-05-12 20:54:04 -0400

    bug fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import style from './Footer.module.sass';
-import { AiOutlineMail,} from 'react-icons/ai';
+import { AiOutlineMail } from 'react-icons/ai';
 import { FiLinkedin } from 'react-icons/fi';
-
+import { FaWhatsapp } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
 	const year = new Date().getFullYear();
@@ -15,12 +16,15 @@ const Footer: React.FC = () => {
 			</div>
 			{/* <span className={style.contact}>Entre em Contato</span> */}
 			<div className={style.footer}>
-				
-				<a className={style.icon} href="mailto:eniorj1@hotmail.com?body=Olá, Tudo bem ?">
+				<a  className={style.icon} href="mailto:eniorj1@hotmail.com?body=Olá, Tudo bem ?">
 					<AiOutlineMail size={40} title="Email" />
 				</a>
-				<a className={style.icon} href="https://www.linkedin.com/in/eniorafael/">
+				<a target="_blank" className={style.icon} href="https://www.linkedin.com/in/eniorafael/">
 					<FiLinkedin size={37} />
+				</a>
+
+				<a target="_blank" className={style.icon} href="https://bit.ly/3hnPSAj">
+					<FaWhatsapp size={37} />
 				</a>
 			</div>
 		</>
